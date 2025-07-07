912a076f96696e8c9d8f04756f95f2fcf8d7758a
hauntmuskie <rioclasher4@gmail.com>
2025-07-08 05:28:02 +0700

    Refactor code structure for improved readability and maintainability

--- src/views/MenuUtama.java
+++ src/views/MenuUtama.java
@@ -36,6 +36,7 @@ public class MenuUtama extends javax.swing.JFrame {
         jButton5 = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -148,10 +149,14 @@ public class MenuUtama extends javax.swing.JFrame {
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 270, 590));
 
+        jLabel2.setBackground(new java.awt.Color(51, 153, 0));
         jLabel2.setFont(new java.awt.Font("SansSerif", 1, 28)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("PENILAIAN KINERJA KURIR J&T EXPRESS");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 330, -1, -1));
+        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel2.setText("PENILAIAN SISWA BERPRESTASI");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 410, -1, -1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/sma_it_raflesia_logo_300.png"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 80, 300, 310));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -245,6 +250,7 @@ public class MenuUtama extends javax.swing.JFrame {
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
